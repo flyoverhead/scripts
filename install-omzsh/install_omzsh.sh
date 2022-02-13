09ca4d8a10119ce1bb506f81943226ed3cfe7082
flypatriot <flypatriotka@gmail.com>
2022-02-13 18:04:03 +0300

    Add omzsh-install script

---
+++ install-omzsh/install_omzsh.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+pd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install omzsh && plugins && themes
+function install_omzsh () {
+   local list=($@)
+   local sum=${#list[@]}
+   for (( n=1; n<$sum; n++ ))
+      do
+         if [[ $(($n % 2)) -eq 0 ]]; then
+            git clone --depth=1 ${list[$n]} $dir
+         else
+            local dir="${list[0]}/${list[$n]}"
+            if [ ! -d $dir ]; then
+               mkdir -p $dir
+            else
+               n=$(($n+1))
+            fi
+         fi
+      done
+}
+
+# Install fonts
+function install_fonts () {
+   local list=($@)
+   local sum=${#list[@]}
+   local dir=${list[0]}
+   for (( url=1; url<$sum; url++ ))
+      do
+         wget -q --show-progress -N ${list[$url]} -P $dir
+      done
+   fc-cache -fv
+}
+
+# Activate plugins
+function define_plugins {
+   local sum=${#custom_plugins[@]}
+   local m=0
+   for (( n=1; n<$sum; n++ ))
+      do
+         if [[ ! $(($n % 2)) -eq 0 ]]; then
+            array[$m]=${custom_plugins[$n]}
+            n=$(($n+1))
+            m=$(($m+1))
+         fi
+      done
+   active_plugins=${array[@]}
+}
+
+# Switching everything ON
+function switch_on {
+   cp "$target_dir/.oh-my-zsh/templates/zshrc.zsh-template" "$target_dir/.zshrc"
+   sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "$target_dir/.zshrc"
+   sed -i "s/plugins=(git)/plugins=(git $active_plugins)/g" "$target_dir/.zshrc"
+   chsh -s /bin/zsh
+}
+
+# Main function
+function main () {
+   source $pd/config
+   install_omzsh ${omzsh[@]}
+   install_omzsh ${plugins[@]}
+   install_omzsh ${custom_plugins[@]}
+   install_omzsh ${custom_themes[@]}
+   install_fonts ${fonts[@]}
+   define_plugins
+   switch_on
+}
+
+main
